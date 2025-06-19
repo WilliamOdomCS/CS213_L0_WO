b716173447126e5d1dfb8c27a42f62e048bea3f3
William Odom <odomwd3773@student.laccd.edu>
2025-06-19 11:47:44 -0700

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,65 @@
+// William Odom
+// Lab #0
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+           String name, strOutput, msgTitle;
+           int num1, num2, sum;
+
+            Scanner scanner = new Scanner(System.in);
+
+            //ImageIcon icon = new ImageIcon("src/JavaLogo.png");
+
+            name = JOptionPane.showInputDialog("What is your full name?");
+
+
+        //JOptionPane.showInputDialog returns a string; we need to convert it to a number before storing in int
+        num1= Integer.parseInt(JOptionPane.showInputDialog("Enter first number"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter second number"));
+
+
+            sum = num1 + num2;
+            msgTitle ="Java program by " + name + "!";
+            strOutput="The sum of " + num1 +
+                     " and " + num2 + " is " + sum;
+
+        //outputs to a console
+        System.out.println(msgTitle +"\n" + strOutput);
+
+        ImageIcon icon = new ImageIcon("src/JavaLogo.png");
+        JOptionPane.showMessageDialog(null, strOutput, msgTitle,
+                JOptionPane.INFORMATION_MESSAGE, icon);
+
+        //showConfirmDialog() will bring up a dialog with the options Yes and No
+        int result = JOptionPane.showConfirmDialog(null,"Are you ready to exit?", "Exit",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+
+        strOutput="You selected: ";
+
+        //switch statement -fall through  demo
+        switch (result)
+        {
+            case JOptionPane.YES_OPTION: strOutput += "YES";
+            case JOptionPane.NO_OPTION: strOutput += "NO";
+            default: strOutput += "\nI am going to exit anyway :) ";
+        }
+
+ System.out.println(strOutput);
+            scanner.close();
+        }
+    }
+
+/*
+Java program by William Odom!
+The sum of 20 and 25 is 45
+You selected: NO
+I am going to exit anyway :) 
+ */
